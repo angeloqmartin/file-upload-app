6fc6dce0ca9a6120480b9e7bf6a86baed3e61670
Angelo Martin <angeloqmartin@gmail.com>
2019-02-05 20:39:16 -0800

    created js file appAccessKeys to hold keys

---
+++ appAccessKeys.js
@@ -0,0 +1,9 @@
+// BucketName, AWS_ACCESS_KEY and AWS_SECRET_ACCESS_KEY should be kept secer
+ const appAccess = {
+    REGION: '',
+    BucketName: '',
+    AWS_ACCESS_KEY: '',
+    AWS_SECRET_ACCESS_KEY: ''
+}
+
+module.exports = appAccess;
